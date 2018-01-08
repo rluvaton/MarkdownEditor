2ef667729825f89e68ab38c522666a6e0028a49e
rluvaton <rluvaton@gmail.com>
2018-01-08 22:01:49 +0200

    Update

--- src/Manager/Main.java
+++ src/Manager/Main.java
@@ -1,9 +1,15 @@
 package Manager;
 
+import Manager.GUI.MarkdownEditorGUI;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        // Test
+        //new MarkdownParser();
 
+        new MarkdownEditorGUI();
+        //new MarkdownEditorGUI();
     }
 }
